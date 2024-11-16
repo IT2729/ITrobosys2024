755c70d9e6262ffcb774a73e966538fb7cb3edb4
Itsuki Terasawa <s21c1083jp@s.chibakoudai.jp>
2024-11-16 18:21:50 +0900

    add a ROF input

--- FKmanipulator.py
+++ FKmanipulator.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
 import numpy as np
 
+while True:
+    try:
+        strDOF = input("自由度を入力してください: ")
+        DOF = int(strDOF)
+    except ValueError as e:
+        print("入力に失敗しました。もう一度数字で入力してください: ")
+    else:
+        break
+
 p1_x = input("0P1のx成分を入力")
 p1_y = input("0P1のy成分を入力")
 p1_z = input("0P1のz成分を入力")
