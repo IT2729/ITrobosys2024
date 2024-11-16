16b6d9f6870b194b8d69dc078a436d348fcc4a3f
Itsuki Terasawa <s21c1083jp@s.chibakoudai.jp>
2024-11-16 19:15:07 +0900

    add a loop of vector r input

--- FKmanipulator.py
+++ FKmanipulator.py
@@ -10,10 +10,38 @@ while True:
     else:
         break
 
-p1_x = input("0P1のx成分を入力")
-p1_y = input("0P1のy成分を入力")
-p1_z = input("0P1のz成分を入力")
-p1 = np.array([[p1_x], [p1_y], [p1_z]])
+for i in range(1, DOF + 1):
+    while True:
+        try:
+            exec(f"input1 =  input('" + str(i - 1) + f"P" + str(i) + f"のx成分を入力してください: ')")
+            vector_r_x = float(input1)
+        except ValueError as e:
+            print("入力に失敗しました。もう一度数字で入力してください: ")
+        else:
+            exec(f"p" + str(i) + f"_x = vector_r_x")
+            break
+        
+    while True:
+        try:
+            exec(f"input1 =  input('" + str(i - 1) + f"P" + str(i) + f"のy成分を入力してください: ')")
+            vector_r_y = float(input1)
+        except ValueError as e:
+            print("入力に失敗しました。もう一度数字で入力してください: ")
+        else:
+            exec(f"p" + str(i) + f"_y = vector_r_y")
+            break
 
-print(p1)
+    while True:
+        try:
+            exec(f"input1 =  input('" + str(i - 1) + f"P" + str(i) + f"のz成分を入力してください: ')")
+            vector_r_z = float(input1)
+        except ValueError as e:
+            print("入力に失敗しました。もう一度数字で入力してください: ")
+        else:
+            exec(f"p" + str(i) + f"_z = vector_r_z")
+            break
+
+    exec(f"p" + str(i) + f"= np.array([[p" + str(i) + f"_x], [p" + str(i) + "_y], [p" + str(i) +"_z]])")
+    exec(f"print(p" + str(i) + f")")
+
 
