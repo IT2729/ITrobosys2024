9d7696e3d8c1d910248a2d53b1759083b1a69d31
Itsuki Terasawa <s21c1083jp@s.chibakoudai.jp>
2024-11-17 00:52:42 +0900

    add determination of FK

--- FKmanipulator.py
+++ FKmanipulator.py
@@ -2,16 +2,19 @@
 import numpy as np
 import sympy as sy
 
+r0 = np.eye(3)
+oPr = sy.zeros(3, 1)
+
 #自由度を設定
 while True:
     try:
         strDOF = input("自由度を入力してください: ")
         DOF = int(strDOF)
     except ValueError as e: #数字以外の入力でループ
-        print("入力に失敗しました。もう一度数字で入力してください: ") 
+        print("入力に失敗しました。もう一度数字で入力してください。") 
     else:
         if DOF > 100:
-            print("自由度が多すぎます。100以下の数字を入力してください")
+            print("自由度が多すぎます。100以下の数字を入力してください。")
         else:
             break
 
@@ -23,7 +26,7 @@ for i in range(1, DOF + 1):
             exec(f"input1 =  input('{str(i - 1)}P{str(i)}のx成分を入力してください: ')")
             vector_r_x = float(input1)
         except ValueError as e: #数字以外の入力でループ
-            print("入力に失敗しました。もう一度数字で入力してください: ")
+            print("入力に失敗しました。もう一度数字で入力してください。")
         else:
             exec(f"p{str(i)}_x = vector_r_x")
             break
@@ -34,7 +37,7 @@ for i in range(1, DOF + 1):
             exec(f"input1 = input('{str(i - 1)}P{str(i)}のy成分を入力してください: ')")
             vector_r_y = float(input1)
         except ValueError as e: #数字以外の入力でループ
-            print("入力に失敗しました。もう一度数字で入力してください: ")
+            print("入力に失敗しました。もう一度数字で入力してください。")
         else:
             exec(f"p{str(i)}_y = vector_r_y")
             break
@@ -45,12 +48,12 @@ for i in range(1, DOF + 1):
             exec(f"input1 =  input('{str(i - 1)}P{str(i)}のz成分を入力してください: ')")
             vector_r_z = float(input1)
         except ValueError as e: #数字以外の入力でループ
-            print("入力に失敗しました。もう一度数字で入力してください: ")
+            print("入力に失敗しました。もう一度数字で入力してください。")
         else:
             exec(f"p{str(i)}_z = vector_r_z")
             break
     
-    exec(f"p{str(i)}= np.array([[p{str(i)}_x], [p{str(i)}_y], [p{str(i)}_z]])") #x,y,z成分から位置ベクトルを生成
+    exec(f"p{str(i)}= sy.Matrix([[p{str(i)}_x], [p{str(i)}_y], [p{str(i)}_z]])") #x,y,z成分から位置ベクトルを生成
     exec(f"print(p{str(i)})") #位置ベクトルを表示
 
     #回転軸ベクトルを設定
@@ -60,24 +63,24 @@ for i in range(1, DOF + 1):
             exec(f"s{str(i)}= np.array([[1], [0], [0]])")
             exec(f"print(s{str(i)})")
             exec(f"sin_theta{str(i)}, cos_theta{str(i)} = sy.symbols('sin_theta{str(i)}, cos_theta{str(i)}')")
-            exec(f"R{str(i)} = np.array([[1, 0, 0], [0, cos_theta{str(i)}, - sin_theta{str(i)}], [0, sin_theta{str(i)}, cos_theta{str(i)}]])")
+            exec(f"r{str(i)} = sy.Matrix([[1, 0, 0], [0, cos_theta{str(i)}, - sin_theta{str(i)}], [0, sin_theta{str(i)}, cos_theta{str(i)}]])")
             break
 
         elif input1 == str("y"):
             exec(f"s{str(i)}= np.array([[0], [1], [0]])")
             exec(f"print(s{str(i)})")
             exec(f"sin_theta{str(i)}, cos_theta{str(i)} = sy.symbols('sin_theta{str(i)}, cos_theta{str(i)}')")
-            exec(f"R{str(i)} = np.array([[cos_theta{str(i)}, 0, sin_theta{str(i)}], [0, 1, 0], [-sin_theta{str(i)}, 0, cos_theta{str(i)}]])")
+            exec(f"r{str(i)} = sy.Matrix([[cos_theta{str(i)}, 0, sin_theta{str(i)}], [0, 1, 0], [-sin_theta{str(i)}, 0, cos_theta{str(i)}]])")
             break
 
         elif input1 == str("z"):
             exec(f"s{str(i)}= np.array([[0], [0], [1]])")
             exec(f"print(s{str(i)})")
             exec(f"sin_theta{str(i)}, cos_theta{str(i)} = sy.symbols('sin_theta{str(i)}, cos_theta{str(i)}')")
-            exec(f"R{str(i)} = np.array([[cos_theta{str(i)}, - sin_theta{str(i)}, 0], [sin_theta{str(i)}, cos_theta{str(i)}, 0], [0, 0 ,1]])")
+            exec(f"r{str(i)} = sy.Matrix([[cos_theta{str(i)}, - sin_theta{str(i)}, 0], [sin_theta{str(i)}, cos_theta{str(i)}, 0], [0, 0 ,1]])")
             break
         else:
-            print("入力に失敗しました。もう一度x,y,zで入力してください: ")
+            print("入力に失敗しました。もう一度x,y,zで入力してください。")
 
 
 #位置ベクトルのx成分を設定
@@ -86,7 +89,7 @@ while True:
         exec(f"input1 =  input('{str(DOF)}Prのx成分を入力してください: ')")
         vector_r_x = float(input1)
     except ValueError as e: #数字以外の入力でループ
-        print("入力に失敗しました。もう一度数字で入力してください: ")
+        print("入力に失敗しました。もう一度数字で入力してください。")
     else:
         exec(f"pr_x = vector_r_x")
         break
@@ -97,7 +100,7 @@ while True:
         exec(f"input1 = input('{str(DOF)}Prのy成分を入力してください: ')")
         vector_r_y = float(input1)
     except ValueError as e: #数字以外の入力でループ
-        print("入力に失敗しました。もう一度数字で入力してください: ")
+        print("入力に失敗しました。もう一度数字で入力してください。")
     else:
         exec(f"pr_y = vector_r_y")
         break
@@ -108,11 +111,23 @@ while True:
         exec(f"input1 =  input('{str(DOF)}Prのz成分を入力してください: ')")
         vector_r_z = float(input1)
     except ValueError as e: #数字以外の入力でループ
-        print("入力に失敗しました。もう一度数字で入力してください: ")
+        print("入力に失敗しました。もう一度数字で入力してください。")
     else:
         exec(f"pr_z = vector_r_z")
         break
     
-exec(f"pr = np.array([[pr_x], [pr_y], [pr_z]])") #x,y,z成分から位置ベクトルを生成
-exec(f"print(pr)") #位置ベクトルを表示
+exec(f"p{str(DOF + 1)} = sy.Matrix([[pr_x], [pr_y], [pr_z]])") #x,y,z成分から位置ベクトルを生成
+exec(f"print(p{str(DOF + 1)})") #位置ベクトルを表示
 
+
+for j in range(0, DOF + 1):
+    k = j
+    exec(f"A = p{str(j + 1)}")
+    while k >= 0:
+        exec(f"A = r{str(k)} * A")
+        k -= 1
+    exec(f"l{str(j)} = A")
+    exec(f"print(l{str(j)})")
+    exec(f"oPr += l{str(j)}")
+
+print(oPr)
