b8ea12943c3c4146214bae96bce1846b2bbd76fe
Itsuki Terasawa <s21c1083jp@s.chibakoudai.jp>
2024-11-16 15:37:00 +0900

    generate matrix test

--- FKmanipulator.py
+++ FKmanipulator.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+import numpy as np
 
-A = input("数字を入力してください:")
-print(A)
+p1_x = input("0P1のx成分を入力")
+p1_y = input("0P1のy成分を入力")
+p1_z = input("0P1のz成分を入力")
+p1 = np.array([[p1_x], [p1_y], [p1_z]])
+
+print(p1)
 
