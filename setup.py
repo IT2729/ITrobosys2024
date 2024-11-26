a49ee0030923c8b62a0a4d6e3bfb15247a255bb6
Itsuki Terasawa <s21c1083jp@s.chibakoudai.jp>
2024-11-27 01:55:39 +0900

    fix

--- setup.py
+++ setup.py
@@ -9,9 +9,6 @@ setup(
     author="Itsuki Terasawa",
     url="https://github.com/IT2729/robosys2024",
     packages=find_packages(),
-    entry_points = {
-        'console_scripts': ['FKmanipulator = FKmanipulator.FKmanipulator:main']
-        },
     python_require='>=3.6',
 )
 
