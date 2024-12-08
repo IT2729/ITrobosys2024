b03d39e1b85edd9e99de713211700014040c9b36
Itsuki Terasawa <s21c1083jp@s.chibakoudai.jp>
2024-12-09 00:23:36 +0900

    change a version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="fkmanipulator",
-    version="0.1.0",
+    version="1.1.0",
     license="BSD-3-Clause",
     author="Itsuki Terasawa",
     url="https://github.com/IT2729/robosys2024",
