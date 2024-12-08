e03b6f64a807ae0626975cd61a3c116f1f7c5b26
Itsuki Terasawa <s21c1083jp@s.chibakoudai.jp>
2024-12-09 01:07:23 +0900

    remove unnecessary files

--- fkmanipulator/__init__.py
+++
@@ -1,9 +0,0 @@
-from .fkmanipulator import *
-
-__copyright__ = 'Copyright (c) 2024 Itsuki Terasawa'
-__version__ = '1.1.0'
-__license__ = 'BSD-3-Clause'
-__author__ = 'Itsuki Terasawa'
-__url__ = 'https://github.com/IT2729/robosys2024'
-
-__all__ = ['fkmanipulator']
